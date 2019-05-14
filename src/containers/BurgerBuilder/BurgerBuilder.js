743906f9acf9a3c28f9e1f4dad58c6fb8cf13d9d
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-05-14 15:42:43 +0530

    order data send to database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,32 +93,12 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You Continue!');
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalprice,
-        //     customer: {
-        //         name: 'Dwip Dutta',
-        //         address: {
-        //             street: 'Teststreet 1',
-        //             zipCOde: '421537',
-        //             country: 'India'
-        //         },
-        //         email: 'dwip@mail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading:false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading:false, purchasing: false});
-        //     });
+        
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios.order';
 
 class ContactData extends Component {
     state = {
@@ -10,12 +11,35 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false 
     }
 
     orderHandler = (event) => {
         event.preventDefault();
-        console.log(this.props.ingredients);
+        // console.log(this.props.price);
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Dwip Dutta',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCOde: '421537',
+                    country: 'India'
+                },
+                email: 'dwip@mail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading:false});
+            })
+            .catch(error => {
+                this.setState({loading:false});
+            });
         
     } 
 
