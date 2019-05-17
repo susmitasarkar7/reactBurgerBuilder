b33a8eeccb7bf32d55bda442dca00974097a37d7
Susmita Sarkar <sarkarsusmita253@gmail.com>
2019-05-17 14:03:10 +0530

    order button managed with redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,11 +38,11 @@ class BurgerBuilder extends Component {
         const sum = Object.keys( ingredients )
             .map( igKey => {
             return ingredients[igKey];
-    } )
-    .reduce( ( sum, el ) => {
-            return sum + el;
-    }, 0 );
-        this.setState( { purchasable: sum > 0 } );
+            } )
+            .reduce( ( sum, el ) => {
+                return sum + el;
+            }, 0 );
+        return sum > 0 ;
     }
 
     purchaseHandler = () => {
@@ -86,7 +86,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.props.onIngredientAdded}
                     ingredientRemoved={this.props.onIngredientRemoved}
                     disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
+                    purchasable={this.updatePurchaseState(this.props.ings)}
                     ordered={this.purchaseHandler}
                     price={this.props.price} />
             </Auxi>
